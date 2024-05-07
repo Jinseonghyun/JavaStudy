f92d2a7901f75d5df4910f01cb6ef2d037a570ef
진성현 <140477594+Jinseonghyun@users.noreply.github.com>
2024-05-08 01:13:48 +0900

    Java_Pratice

---
+++ java_practice/src/variable/Var6.java
@@ -0,0 +1,8 @@
+package variable;
+
+public class Var6 {
+    public static void main(String[] args) {
+        int a;
+        //System.out.println(a); // 선언 했지만 초기화를 안했다 컴파이 에러
+    }
+}
---
+++ java_practice/src/variable/ex/VarEx3.java
@@ -0,0 +1,12 @@
+package variable.ex;
+
+public class VarEx3 {
+    public static void main(String[] args) {
+        long longVar = 1000000000L;
+        System.out.println("longVar = " + longVar); // soutv 가까운 변수값을 읽어서 가져옴
+        System.out.println(longVar);
+
+        boolean booleanVar = true;
+        System.out.println(booleanVar);
+    }
+}
